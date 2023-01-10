11ebff5903728713c51c5b30e6ef321ce9bed2b3
Vasyuk Vladimir <vasiuk.va@phystech.edu>
2023-01-11 00:04:54 +0300

    Add a bit of info in data plotting

--- data_renderer.py
+++ data_renderer.py
@@ -18,15 +18,24 @@ def show_graph_with_labels(adjacency_matrix):
     plt.show()
 
 
-def render_data_mugs(vertexes_data, colours_data):
+def render_data_mugs(vertexes_data, colours_data, asymp_mugs):
     plt.scatter(vertexes_data, colours_data, c='red', alpha=ALPHA, edgecolors='none')
-    plt.plot([i for i in range(MAX_EMBEDDING_ITERATIONS*11)], [(j ** 0.5) * 3 for j in range(MAX_EMBEDDING_ITERATIONS*11)])
-    plt.plot([i for i in range(MAX_EMBEDDING_ITERATIONS*11)], [(j ** 0.5) * 2.1 for j in range(MAX_EMBEDDING_ITERATIONS*11)])
+    plt.plot([i for i in range((MAX_EMBEDDING_ITERATIONS+1)*12)], [(j ** 0.5) * 3 for j in range((MAX_EMBEDDING_ITERATIONS+1)*12)])
+    plt.plot([i for i in range((MAX_EMBEDDING_ITERATIONS+1)*12)], [(j ** 0.5) * asymp_mugs for j in range((MAX_EMBEDDING_ITERATIONS+1)*12)], 'black')
+    plt.xlabel("Number of vertexes is graph")
+    plt.ylabel("Number of used colours")
+    plt.title("Colours usage, generation with embedding")
+    plt.grid()
     plt.show()
 
 
-def render_data_random(vertexes_data, colours_data, average_random):
+def render_data_random(vertexes_data, colours_data, asymp_random, average_random):
     plt.scatter(vertexes_data, colours_data, c='green', alpha=ALPHA, edgecolors='none')
-    plt.plot([i for i in range(VERTEXES_MAX)], [(j ** 0.5) * 3 for j in range(VERTEXES_MAX)])
-    plt.plot([i for i in range(VERTEXES_MAX)], [(j ** 0.5) * average_random for j in range(VERTEXES_MAX)],'r')
+    plt.plot([i for i in range(VERTEXES_MAX+1)], [(j ** 0.5) * 3 for j in range(VERTEXES_MAX+1)])
+    plt.plot([i for i in range(VERTEXES_MAX+1)], [(j ** 0.5) * asymp_random for j in range(VERTEXES_MAX+1)], 'black')
+    plt.plot([i for i in range(VERTEXES_MAX+1)], [(j ** 0.5) * average_random for j in range(VERTEXES_MAX+1)],'r')
+    plt.xlabel("Number of vertexes is graph")
+    plt.ylabel("Number of used colours")
+    plt.title("Colours usage, random generation")
+    plt.grid()
     plt.show()
--- main.py
+++ main.py
@@ -38,12 +38,13 @@ def run_once_with_random_generator():
 def run():
     vertexes_mugs, colours_mugs = np.empty([GRAPHS_TO_GENERATE], dtype=int), np.empty([GRAPHS_TO_GENERATE], dtype=int)
     vertexes_random, colours_random = np.empty([GRAPHS_TO_GENERATE], dtype=int), np.empty([GRAPHS_TO_GENERATE], dtype=int)
-    asymp_mugs, asymp_random, average_random = 0, 0, 0
+    asymp_mugs, asymp_random, average_MUGs, average_random = 0, 0, 0, 0
 
     for i in tqdm(range(GRAPHS_TO_GENERATE)):
         vertexes, colours = run_once_with_MUGs()
         n = colours/(vertexes ** 0.5)
         asymp_mugs = n if n > asymp_mugs else asymp_mugs
+        average_MUGs += n/GRAPHS_TO_GENERATE
         vertexes_mugs[i] = vertexes
         colours_mugs[i] = colours
 
@@ -54,9 +55,10 @@ def run():
         vertexes_random[i] = vertexes
         colours_random[i]= colours
 
-    render_data_mugs(vertexes_mugs, colours_mugs)
-    render_data_random(vertexes_random, colours_random, average_random)
+    render_data_mugs(vertexes_mugs, colours_mugs, asymp_mugs)
+    render_data_random(vertexes_random, colours_random, asymp_random, average_random)
     print("Asymptotic is {} for MUGs generator and {} for random generator".format(asymp_mugs, asymp_random))
+    print("Average is {} for MUGs generator and {} for random generator".format(average_MUGs, average_random))
 
 if __name__ == '__main__':
     run()
